76177308c124322309c38456476d779c7e38d6ff
spyre <yssaketh@gmail.com>
2023-10-14 17:48:30 -0400

    ripped panolens

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+const PANOLENS = require("panolens");
+
+var app, panorama, viewer, infospot;
+
+app = document.querySelector("#app");
+
+
+viewer = new PANOLENS.Viewer({
+    enableReticle: false,
+    container: app,
+    output: "console",
+    viewIndicator: true,
+});
+
+panorama = new PANOLENS.ImagePanorama(
+    'https://firebasestorage.googleapis.com/v0/b/chalk-bot.appspot.com/o/res.jpg?alt=media&token=1b96c6ea-a503-49cb-9c17-adcc2afd5d25&_gl=1*e1f3ay*_ga*MTk4MjI3NzU5OC4xNjk0NjU0MzUz*_ga_CW55HF8NVT*MTY5NzI0NjQzNy4yMS4xLjE2OTcyNDY0NzkuMTguMC4w'
+    /* The commented line `//
+    "https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg"` is
+    specifying the URL of an equirectangular image that will be used as the panorama for the viewer. */
+    // "https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg"
+    // '/Users/dhruvroongta/Downloads/a-landscape-with-hills-and-trees-hvwpfxfz.jpeg'
+//    ' http://127.0.0.1:8080/Unknown-8.jpeg'
+// 'Unknown-8.jpg'
+// 'https://images.unsplash.com/photo-1596263576925-d90d63691097?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8MzYwfGVufDB8fDB8fHww&auto=format&fit=crop&w=900&q=60'
+    //"https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg"
+);
+
+infospot = new PANOLENS.Infospot(350, PANOLENS.DataImage.Info);
+infospot.position.set(0, 0, 0);
+
+panorama.add(infospot);
+
+viewer.add(panorama);
