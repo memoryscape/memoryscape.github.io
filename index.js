85e54ea521fd64022aa0a9aa920512b3576c8320
Dhruv317 <droongta05@gmail.com>
2023-10-14 23:54:56 -0400

    Built, updated pic to soccer pic

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ viewer = new PANOLENS.Viewer({
 });
 
 panorama = new PANOLENS.ImagePanorama(
-    'https://firebasestorage.googleapis.com/v0/b/chalk-bot.appspot.com/o/res.jpg?alt=media&token=1b96c6ea-a503-49cb-9c17-adcc2afd5d25&_gl=1*e1f3ay*_ga*MTk4MjI3NzU5OC4xNjk0NjU0MzUz*_ga_CW55HF8NVT*MTY5NzI0NjQzNy4yMS4xLjE2OTcyNDY0NzkuMTguMC4w'
+    'https://firebasestorage.googleapis.com/v0/b/memoryscape-59213.appspot.com/o/result.jpg?alt=media&token=dc7afe49-9e11-4428-9f06-336b79254fd8&_gl=1*1g0bpet*_ga*MjkyNzgzNDQuMTY5NzIzNzI3OQ..*_ga_CW55HF8NVT*MTY5NzM0MTEzMi45LjEuMTY5NzM0MTE0NC40OC4wLjA'
+    // 'https://firebasestorage.googleapis.com/v0/b/chalk-bot.appspot.com/o/res.jpg?alt=media&token=1b96c6ea-a503-49cb-9c17-adcc2afd5d25&_gl=1*e1f3ay*_ga*MTk4MjI3NzU5OC4xNjk0NjU0MzUz*_ga_CW55HF8NVT*MTY5NzI0NjQzNy4yMS4xLjE2OTcyNDY0NzkuMTguMC4w'
     /* The commented line `//
     "https://pchen66.github.io/Panolens/examples/asset/textures/equirectangular/tunnel.jpg"` is
     specifying the URL of an equirectangular image that will be used as the panorama for the viewer. */
